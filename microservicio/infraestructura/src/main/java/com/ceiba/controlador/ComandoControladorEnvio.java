eb1ffc871ea69e3b30e06afe28e03f1e09c2d03e
AndresEduVillamizar <eduardo.villamizar@ceiba.com.co>
2022-02-15 19:20:20 -0500

    Jmeter

--- microservicio/infraestructura/src/main/java/com/ceiba/controlador/ComandoControladorEnvio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/controlador/ComandoControladorEnvio.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/envios")
 @Api(tags = { "Controlador comando envios"})
+@CrossOrigin(origins = "http://localhost:4200")
 public class ComandoControladorEnvio {
 
     private final ManejadorCrearUsuarioEnvio manejadorCrearUsuarioEnvio;
